c40d2834dea4b60581e88621ab7fef5559b2f35e
alikurniawan <alikurniawan02@gmail.com>
2020-05-05 15:41:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from "react";
-import { Jumbotron, Row, Col } from "reactstrap";
-import logo from "./logo.svg";
-import "./App.css";
+import Header from './components/nHeader';
 
 function App() {
   return (
     <div>
-      <Jumbotron fluid style={{ height: "5px" }}>
-        <Row>
-          <Col xs="1">
-            <img
-              src={logo}
-              style={{ width: "150px", marginTop: "-50px" }}
-              className="App-logo"
-              alt="logo"
-            />
-          </Col>
-          <Col>
-            <p style={{marginLeft: '13px', marginTop: '-19px'}}><h2>Small Portfolio</h2></p>
-          </Col>
-        </Row>
-      </Jumbotron>
+      <Header />
     </div>
   );
 }
---
+++ src/components/nHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Jumbotron, Row, Col } from "reactstrap";
+import logo from "../logo.svg";
+import "../App.css";
+
+const nHeader = () => (
+    <Jumbotron fluid style={{ height: "5px" }}>
+        <Row>
+          <Col xs="1">
+            <img
+              src={logo}
+              style={{ width: "150px", marginTop: "-50px" }}
+              className="App-logo"
+              alt="logo"
+            />
+          </Col>
+          <Col>
+            <p style={{marginLeft: '13px', marginTop: '-19px'}}><h2>Small Portfolio</h2></p>
+          </Col>
+        </Row>
+      </Jumbotron>
+)
+export default nHeader;
