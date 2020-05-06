c3b192fdaa3807248450534ecefd8b6a6480465c
alikurniawan <alikurniawan02@gmail.com>
2020-05-06 11:16:10 +0700

    update

--- src/components/cardButton.jsx
+++ src/components/cardButton.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import {Label, Input} from 'reactstrap';
 
 class cardButton extends Component {
-  state = {};
+ constructor(){
+   super();
+ }
   render() {
     return (
       <div>
@@ -9,10 +12,15 @@ class cardButton extends Component {
           <div class="card-header">PortFolio 2</div>
           <div class="card-body">
             <h4 class="card-title">Button Search</h4>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+            <div>
+              <Label>Username</Label>
+              <Input
+                type="text"
+                class="form-control"
+                name="username"
+                value=""
+              />
+            </div>
           </div>
         </div>
       </div>
